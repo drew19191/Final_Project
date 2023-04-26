fea4bb289752872421196234a1a37d0c64ed4555
drew19191 <117569464+drew19191@users.noreply.github.com>
2023-04-25 21:06:37 -0700

    templates

--- index.js
+++
@@ -1,9 +0,0 @@
-console.log("hi");
-
-$("#toggle").on("click", function(){
-    $("#dropDown").slideToggle();
-  });
-  
-  $(".ref").on("click", function(){
-    $("#dropDown2").slideToggle();
-  });
